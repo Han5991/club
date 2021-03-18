0e0826a8a8cd7d226848be0ebe3be88843d3f099
Han Sang-Wook <73056306+Han5991@users.noreply.github.com>
2021-03-18 09:48:11 +0900

    [Part5] ~ P500 login

---
+++ src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package org.zerock.club.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@Log4j2
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //사용자 계정은 user1
+        auth.inMemoryAuthentication().withUser("user1")
+                //1111 패스워드 인코딩 결과
+                .password("$2a$10$/lhrnu9kMFGTwjMI5CWmTuGeerK11w35XZWar8DZ09E2K.DMOQPZ6")
+                .roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/sample/all").permitAll()
+                .antMatchers("/sample/member")
+                .hasRole("user");
+        http.formLogin();//인가/인증에 문제시 로그인 화면
+        http.csrf().disable();
+    }
+}
