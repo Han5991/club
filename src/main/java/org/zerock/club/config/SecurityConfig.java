4692a194d4fe4ec57a259fb5a39f192a253bedb7
Han Sang-Wook <73056306+Han5991@users.noreply.github.com>
2021-03-20 06:47:24 +0900

    [Part5] ~ P536 oauth2 google 개신기

--- src/main/java/org/zerock/club/config/SecurityConfig.java
+++ src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/sample/member")
                 .hasRole("USER");
         http.formLogin();//인가/인증에 문제시 로그인 화면
-//        http.csrf().disable();
-//        http.logout();//로그아웃
+        http.csrf().disable();
+        http.oauth2Login();
     }
 }
